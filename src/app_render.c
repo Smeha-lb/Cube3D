5ecb7653177990f313016942307947ff5ee67a06
Carl Samaha <carlsamaha1@gmail.com>
2025-09-08 11:01:15 +0300

    shortened a function over 25 lines

--- src/app_render.c
+++ src/app_render.c
@@ -35,6 +35,31 @@ static void	draw_background(t_image *img, int ceil_color, int floor_color)
 	draw_half(img, WIN_H / 2, WIN_H, floor_color);
 }
 
+static void	update_animations(t_app *app)
+{
+	struct timeval tv;
+	long now;
+	long dt_ms;
+
+	gettimeofday(&tv, NULL);
+	now = (long)tv.tv_sec * 1000L + (long)(tv.tv_usec / 1000L);
+	dt_ms = now - app->last_anim_time_ms;
+	if (dt_ms < 0)
+		dt_ms = 0;
+	app->last_anim_time_ms = now;
+	app->torch_anim_accum_ms = app->torch_anim_accum_ms + dt_ms;
+	while (app->torch_anim_accum_ms >= app->torch_frame_ms)
+	{
+		app->torch_anim_accum_ms = app->torch_anim_accum_ms - app->torch_frame_ms;
+		if (app->cfg.torch_frame_count > 0)
+		{
+			app->torch_frame_index = app->torch_frame_index + 1;
+			if (app->torch_frame_index >= app->cfg.torch_frame_count)
+				app->torch_frame_index = 0;
+		}
+	}
+}
+
 int	render_loop(void *param)
 {
 	t_app	*app;
@@ -42,29 +67,7 @@ int	render_loop(void *param)
 	app = (t_app *)param;
 	if (!app->running)
 		return (0);
-	{
-		struct timeval tv;
-		long now;
-		long dt_ms;
-
-		gettimeofday(&tv, NULL);
-		now = (long)tv.tv_sec * 1000L + (long)(tv.tv_usec / 1000L);
-		dt_ms = now - app->last_anim_time_ms;
-		if (dt_ms < 0)
-			dt_ms = 0;
-		app->last_anim_time_ms = now;
-		app->torch_anim_accum_ms = app->torch_anim_accum_ms + dt_ms;
-		while (app->torch_anim_accum_ms >= app->torch_frame_ms)
-		{
-			app->torch_anim_accum_ms = app->torch_anim_accum_ms - app->torch_frame_ms;
-			if (app->cfg.torch_frame_count > 0)
-			{
-				app->torch_frame_index = app->torch_frame_index + 1;
-				if (app->torch_frame_index >= app->cfg.torch_frame_count)
-					app->torch_frame_index = 0;
-			}
-		}
-	}
+	update_animations(app);
 	update_player(app);
 	draw_background(&app->frame, app->cfg.ceiling.argb, app->cfg.floor.argb);
 	raycast_frame(app);
